de56c0b539dcd0f36b971938dff2ba3aa32f00df
O1egik90 <leg.4@inbox.ru>
2023-01-30 20:56:42 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,38 @@
+
+    <!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <title>TASK 12</title>
+</head>
+<body>
+<?php
+    include 'persons.php';
+    include 'script.php';  
+    echo 'Исходный массив:'.'<br>';  
+    print_r($example_persons_array); 
+    echo '<br>';  
+    echo '<br>';  
+    $fullName = $example_persons_array[0]['fullname'];
+    echo "Исходное полное имя:".$fullName.'<br>';
+    echo '<br>';  
+    $chunckedName = getPartsFromFullname($example_persons_array[0]['fullname']);
+    echo "Результат работы getPartsFromFullname:";
+    print_r($chunckedName); 
+    echo '<br>';
+    echo '<br>';  
+    echo "Результат работы getPartsFromFullname:".getFullnameFromParts($chunckedName['surname'],$chunckedName['name'],$chunckedName['patronomyc']).'<br>';     
+    echo '<br>';  
+    echo "Результат работы getShortName:".getShortName($fullName).'<br>';
+    echo '<br>';  
+    echo "Результат работы getGenderFromName:".getGenderFromName($fullName).'<br>';
+    echo '<br>';  
+    echo 'Гендерный состав аудитории:';
+    echo '<br>';
+    getGenderDescription($example_persons_array);
+    echo '<br>';  
+    getPerfectPartner("АЛиеВ","ПеТр","АфаНасиевИЧ",$example_persons_array);
+?>
+   
+</body>
+</html>
